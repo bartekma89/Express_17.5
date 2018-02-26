2387a10bef02b656b87ecd4ce368db4d2d13725b
Bartek <bartekma89@gmail.com>
2018-02-26 14:07:05 +0100

    use middleware

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use('/store', function(req, res, next) {
+	console.log('I am middleman at the request to /store');
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello');
+});
+
+app.get('/store', function(req, res) {
+	res.send('This is store');
+});
+
 app.listen(3000);
+
+app.use(function(req, res, next) {
+	res.status(404).send('Invalid url');
+});
